2255577586d2c2dba675c4a6772452908f0933ca
unknown <khakurelsunita1234@g>
2023-03-23 08:46:55 +0545

    changed program2.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,15 @@ namespace firstapp
         static void Main(string[] args)
         {
             Console.WriteLine("hello this is sunita ");
-            console.Readline();
+            string wlcomenote = Console.ReadLine();
+            console.WriteLine(welcomenote);
+            console.Clear();
+            console.WriteLine("enter first number");
+            int firstnumber=convert.ToInt32(console.Readline());
+            console.WriteLine("enter second number");
+            int secondnumber=convert.toint32(console.Readline());
+            int sum = firstnumber+secondnumber;
+            console.WriteLine("sum of two number is" + sum);
             Console.ReadKey();
         }
     }
---
+++ program2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //        Console.WriteLine("hello this is sunita ");
+            //        string wlcomenote = Console.ReadLine();
+            //        Console.WriteLine(wlcomenote);
+            //        Console.Clear();
+            //        Console.WriteLine("enter first number");
+            //        int firstnumber = Convert.ToInt32(Console.ReadLine());
+            //        Console.WriteLine("enter second number");
+            //        int secondnumber = Convert.ToInt32(Console.ReadLine());
+            //        int sum = firstnumber + secondnumber;
+            //        Console.WriteLine("sum of two number is" + sum);
+            //        Console.WriteLine("Do you want to quite if yes press Q");
+            //        char a = Console.ReadLine()[0];
+            //        bool quit = false;
+            //        if (a == 'Q')
+            //        { quit = true; }
+
+
+            //        if (quit == false)
+            //        {
+            //            Console.ReadKey();
+
+            //        }
+
+
+
+
+            //        Console.ReadKey();
+            //        Int16 mobileNumber = 1254;
+            //        float number = 1.5F;
+
+            //        Console.ReadKey();
+            //    }
+
+            string myName = "sunita khakurel";
+            Console.WriteLine(myName.ToUpper());
+            Console.WriteLine(myName.ToLower());
+            Console.ReadKey();
+        }
+    }
+}
