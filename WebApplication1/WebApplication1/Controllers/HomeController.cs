e2ea0cd7e4c8d889660de1826ecf1aefd8af9bae
unknown <khakurelsunita1234@g>
2023-04-06 08:47:48 +0545

    changed sunita.net

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult sunita()
+        {
+            return View();
+        }
+        public IActionResult suman()
+        {
+            return View();
+        }
+        public IActionResult amy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+       
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class NistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
