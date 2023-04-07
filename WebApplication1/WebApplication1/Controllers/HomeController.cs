fe1b30728435635771b2c56567a7e277ccccde2b
unknown <khakurelsunita1234@g>
2023-04-07 08:50:53 +0545

    changed sunita.net

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -4,42 +4,77 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		//[HttpGet]
+		public ViewResult Index()
+		{
+			return View();
+			//return RedirectToAction("sunita");
+			//return Content("i am sunita");
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        public IActionResult sunita()
-        {
-            return View();
-        }
-        public IActionResult suman()
-        {
-            return View();
-        }
-        public IActionResult amy()
-        {
-            return View();
-        }
+			}
+			//public RedirectToActionResult Index()
+			//{
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-       
-    }
+			//	return RedirectToAction("sunita");
+
+
+			//}
+			//public IActionResult Index()
+			//{
+
+			//	return Content("i am sunita");
+
+			//}
+			public JsonResult JsonResult()
+		{
+			
+			return Json(
+				new
+				{
+					name="sunita", Class="csit" , rollNo=34
+				});
+
+		}
+		public FileResult Download()
+		{
+			string name = "myfiles/sunita.txt";
+			string ext = "image/png";
+			return File("myfiles/sunita.jpg",ext, name);
+		}
+
+
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+		public IActionResult sunita()
+		{
+			return View();
+		}
+		public IActionResult suman()
+		{
+			return View();
+		}
+		public IActionResult amy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+	}
 }
